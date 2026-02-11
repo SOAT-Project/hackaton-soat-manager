61568af592dd5e67cb3d552b7d23cbd0efbc60da
srteixeiradias <samuelteixeiradias008@gmail.com>
2026-02-10 22:33:55 -0300

    initial commit

---
+++ src/main/java/soat/project/hackaton_soat_manager/HackatonSoatManagerApplication.java
@@ -0,0 +1,14 @@
+package soat.project.hackaton_soat_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonSoatManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonSoatManagerApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/soat/project/hackaton_soat_manager/HackatonSoatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package soat.project.hackaton_soat_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonSoatManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
