fa30cfc2e122e1fd7bc619c39d9c3364d2146a3b
srteixeiradias <samuelteixeiradias008@gmail.com>
2026-02-17 20:05:26 -0300

    feat(video) : add download model

---
+++ src/main/java/soat/project/hackaton_soat_manager/infrastructure/video/presenter/DownloadVideoPresenter.java
@@ -0,0 +1,15 @@
+package soat.project.hackaton_soat_manager.infrastructure.video.presenter;
+
+import soat.project.hackaton_soat_manager.application.output.video.download.DownloadVideoOutput;
+import soat.project.hackaton_soat_manager.infrastructure.video.model.response.DownloadVideoResponse;
+
+public interface DownloadVideoPresenter {
+
+    static DownloadVideoResponse present(DownloadVideoOutput output) {
+        return new DownloadVideoResponse(
+                output.downloadUrl(),
+                output.expiresAt(),
+                output.fileName()
+        );
+    }
+}
