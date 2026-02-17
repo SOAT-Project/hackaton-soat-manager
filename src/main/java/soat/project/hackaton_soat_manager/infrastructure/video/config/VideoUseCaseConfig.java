52ca10c8bf705b8a9418d1dcdcc6b121825656a5
srteixeiradias <samuelteixeiradias008@gmail.com>
2026-02-17 16:31:58 -0300

    feat(video) : add video usecase config

---
+++ src/main/java/soat/project/hackaton_soat_manager/infrastructure/video/config/VideoUseCaseConfig.java
@@ -0,0 +1,36 @@
+package soat.project.hackaton_soat_manager.infrastructure.video.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import soat.project.hackaton_soat_manager.application.gateway.NotificationQueueGateway;
+import soat.project.hackaton_soat_manager.application.gateway.ProcessingQueueGateway;
+import soat.project.hackaton_soat_manager.application.gateway.StorageGateway;
+import soat.project.hackaton_soat_manager.application.gateway.VideoProcessingGateway;
+import soat.project.hackaton_soat_manager.application.usecase.video.update.UpdateVideoStatusUseCase;
+import soat.project.hackaton_soat_manager.application.usecase.video.update.UpdateVideoStatusUseCaseImpl;
+import soat.project.hackaton_soat_manager.application.usecase.video.upload.UploadVideoUseCase;
+import soat.project.hackaton_soat_manager.application.usecase.video.upload.UploadVideoUseCaseImpl;
+
+@Configuration
+public class VideoUseCaseConfig {
+
+    @Bean
+    public UploadVideoUseCase uploadVideoUseCase(
+            VideoProcessingGateway videoProcessingGateway,
+            StorageGateway storageGateway,
+            ProcessingQueueGateway processingQueueGateway,
+            @Value("${aws.s3.bucket}") String bucket
+    ){
+        return new UploadVideoUseCaseImpl(videoProcessingGateway, storageGateway, processingQueueGateway, bucket);
+    }
+
+    @Bean
+    public UpdateVideoStatusUseCase updateVideoStatusUseCase(
+            VideoProcessingGateway videoProcessingGateway,
+            NotificationQueueGateway notificationQueueGateway
+    ){
+        return new UpdateVideoStatusUseCaseImpl(videoProcessingGateway, notificationQueueGateway);
+    }
+
+}
