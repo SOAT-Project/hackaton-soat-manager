7bdca952ccbe0752693c31f0e9fbe5bd9e1245e9
srteixeiradias <samuelteixeiradias008@gmail.com>
2026-02-17 20:06:33 -0300

    feat(s3) : add url presigner for s3 video download

--- src/main/java/soat/project/hackaton_soat_manager/infrastructure/config/AwsConfig.java
+++ src/main/java/soat/project/hackaton_soat_manager/infrastructure/config/AwsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 
 @Configuration
@@ -18,5 +19,11 @@ public class AwsConfig {
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder().build();
     }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder().build();
+    }
+
 }
 
--- src/main/java/soat/project/hackaton_soat_manager/infrastructure/web/s3/S3StorageGateway.java
+++ src/main/java/soat/project/hackaton_soat_manager/infrastructure/web/s3/S3StorageGateway.java
@@ -6,22 +6,29 @@ import org.springframework.stereotype.Component;
 import soat.project.hackaton_soat_manager.application.gateway.StorageGateway;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import java.io.InputStream;
+import java.time.Duration;
 
 @Component
 public class S3StorageGateway implements StorageGateway {
 
     private final S3Client s3Client;
     private final String defaultBucket;
+    private final S3Presigner s3Presigner;
 
     public S3StorageGateway(
             final S3Client s3Client,
-            @Value("${aws.s3.bucket}") final String defaultBucket
+            @Value("${aws.s3.bucket}") final String defaultBucket,
+            final S3Presigner s3Presigner
     ) {
         this.s3Client = s3Client;
         this.defaultBucket = defaultBucket;
+        this.s3Presigner = s3Presigner;
     }
 
     @Override
@@ -60,6 +67,18 @@ public class S3StorageGateway implements StorageGateway {
 
     @Override
     public String generateDownloadUrl(String bucket, String key) {
-        return "";
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        PresignedGetObjectRequest presignedRequest =
+                s3Presigner.presignGetObject(r -> r
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .getObjectRequest(getObjectRequest)
+                );
+
+        return presignedRequest.url().toString();
     }
 }
