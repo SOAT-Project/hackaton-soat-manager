0a5ae445d19cced1b40ab3cf58779bc2ac80696f
srteixeiradias <samuelteixeiradias008@gmail.com>
2026-02-17 16:25:43 -0300

    feat(video) : add domain repository gateway

---
+++ src/main/java/soat/project/hackaton_soat_manager/application/gateway/VideoProcessingGateway.java
@@ -0,0 +1,22 @@
+package soat.project.hackaton_soat_manager.application.gateway;
+
+import soat.project.hackaton_soat_manager.domain.video.ProcessId;
+import soat.project.hackaton_soat_manager.domain.video.UserId;
+import soat.project.hackaton_soat_manager.domain.video.VideoProcessing;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VideoProcessingGateway {
+
+    VideoProcessing save(VideoProcessing video);
+
+    Optional<VideoProcessing> findByUserIdAndProcessId(
+            UserId userId,
+            ProcessId processId
+    );
+
+    Optional<VideoProcessing> findByProcessId(ProcessId processId);
+
+    List<VideoProcessing> findAllByUserId(UserId userId);
+}
